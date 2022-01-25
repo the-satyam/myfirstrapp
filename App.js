596370dbfbb1c635c1f848d1c8951bb24d3f2436
the-satyam <satyamvishwa03@gmail.com>
2022-01-25 14:31:53 +0530

    updating in component style

--- App.js
+++ App.js
@@ -9,106 +9,13 @@ import {
   Button,
   TouchableOpacity,
 } from "react-native";
+import Login from "./components/Login";
 
 export default function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+ 
 
   return (
-    <View style={styles.container}>
-    <Text style = {styles.heading}>Bajaj Training</Text>
-     <Text style = {styles.salutation}>Welcome All!</Text>
-
-      <StatusBar style="auto" />
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email or Username"
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-        />
-      </View>
-
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Password"
-          placeholderTextColor="#003f5c"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        />
-      </View>
-
-      <TouchableOpacity>
-        <Text style={styles.forgot_button}>Forgot Password?</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.loginBtn}>
-        <Text style={styles.loginText}>LOGIN</Text>
-      </TouchableOpacity>
-      <Text style = {styles.or}>OR</Text>
-      <TouchableOpacity style={styles.loginBtn}>
-      <Text style={styles.loginText}>Sign Up</Text>
-    </TouchableOpacity>
-    </View>
+   <Login/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    
-  },
-
-  
-
-  inputView: {
-    backgroundColor: "yellow",
-    borderRadius: 30,
-    width: "70%",
-    height: 45,
-    marginBottom: 20,
-
-    alignItems: "center",
-  },
-
-  TextInput: {
-    height: 50,
-    flex: 1,
-    padding: 10,
-    marginLeft: 20,
-  },
-
-  forgot_button: {
-    height: 30,
-    marginBottom: 30,
-  },
-
-  loginBtn: {
-    width: "30%",
-    borderRadius: 25,
-    height: 40,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 20,
-    backgroundColor: "#78E1E8",
-  },
-  heading: {
-    marginBottom: 20,
-    fontWeight: "bold",
-    fontSize: 50,
-    color: "blue",
-    fontFamily: "sans-serif-condensed",
-  },
-  salutation: {
-    marginBottom: 70,
-    fontSize: 30,
-    color: "purple",
-  },
-  or: {
-    marginTop: 20,
-  },
-});
